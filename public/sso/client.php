04868d8debc2fbe84a36603681270a1f51566f09
Erin3012 <ericxxnn20@gmail.com>
2026-09-23 21:51:55 -0300

    Corregir ruta publica del SSO

--- public/sso/client.php
+++ public/sso/client.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '/../app/bootstrap.php';
+require_once __DIR__ . '/../../app/bootstrap.php';
 
 function sso_exchange(string $code, string $clientId): array
 {
